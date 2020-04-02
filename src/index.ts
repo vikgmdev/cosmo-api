824fef615b43bc8cc4b556891201e806ee0b5d34
Victor Garcia <vikgm.dev@gmail.com>
2020-04-02 15:33:50 -0600

    Fix linter tabs

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { logger } from './utils';
 import app from './app';
 
 app.listen({ port: config.port }, (): void => {
-    logger.info(`🚀 ${config.name} up and running in ${config.env} @ http://localhost:${config.port}`);
+  logger.info(`🚀 ${config.name} up and running in ${config.env} @ http://localhost:${config.port}`);
 });
