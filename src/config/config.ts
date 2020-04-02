d8cd6cf788e4748913bc98dd5749be407e9beb50
Victor Garcia <vikgm.dev@gmail.com>
2020-04-02 15:20:22 -0600

    Change default port

--- src/config/config.ts
+++ src/config/config.ts
@@ -18,7 +18,7 @@ interface Config {
     env: process.env.NODE_ENV || 'development',
     logLevel: process.env.LOG_LEVEL || 'debug',
     debug: Boolean(process.env.DEBUG) || true,
-    port: Number(process.env.PORT) || 3000,
+    port: Number(process.env.PORT) || 4000,
     version: process.env.API_VERSION || '1.0.0'
   };
   
