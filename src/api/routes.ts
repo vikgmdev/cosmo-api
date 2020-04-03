25337258f16f633ccd2894343b02a68ee0cad4bc
Victor Garcia <vikgm.dev@gmail.com>
2020-04-03 12:38:16 -0600

    Add user route

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,10 @@
 import { Application } from 'express';
 import AuthRoutes from './auth/auth.routes';
 import HealthRoutes from './health/health.routes';
+import UserRoutes from './user/user.routes';
 
 export default function routes(app: Application): void {
   app.use('/auth', AuthRoutes);
   app.use('/health', HealthRoutes);
+  app.use('/user', UserRoutes);
 }
