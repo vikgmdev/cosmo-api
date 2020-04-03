07a5408f8d906ab0df9d9f0291e6460b5420aa04
Victor Garcia <vikgm.dev@gmail.com>
2020-04-02 22:46:29 -0600

    Add body parser and routes to express app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import { config } from './config';
+import routes from './routes';
 
 const app = express();
+
+app.use(bodyParser.json({ limit: config.requestLimit }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: config.requestLimit,
+  }),
+);
+routes(app);
 
 export default app;
