4c694a228f630b9a5036a0899c5d3c7679fa169d
Victor Garcia <vikgm.dev@gmail.com>
2020-04-03 12:05:24 -0600

    Add auth endpoint and routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,6 +1,8 @@
 import { Application } from 'express';
+import AuthRoutes from './auth/auth.routes';
 import HealthRoutes from './health/health.routes';
 
 export default function routes(app: Application): void {
+  app.use('/auth', AuthRoutes);
   app.use('/health', HealthRoutes);
 }
