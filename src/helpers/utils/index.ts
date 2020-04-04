83e10c5bce6d3e86c6e9e2952035d2daec613316
Victor Garcia <vikgm.dev@gmail.com>
2020-04-03 23:24:54 -0600

    Add new utils

--- src/helpers/utils/index.ts
+++ src/helpers/utils/index.ts
@@ -1,5 +1,13 @@
-import { buildPaginationQuery } from './query-builder';
+import * as passwords from './passwords';
+import addressFromRequest from './address-from-request';
+import buildPaginationQuery from './query-builder';
+import randomString from './random-string';
+import sendTemplateEmail from './send-template-email';
 
 export default {
+  addressFromRequest,
   buildPaginationQuery,
+  passwords,
+  randomString,
+  sendTemplateEmail,
 };
--- src/helpers/utils/query-builder.ts
+++ src/helpers/utils/query-builder.ts
@@ -1,6 +1,6 @@
 import { PaginationQuery } from '../../types';
 
-export function buildPaginationQuery(query: PaginationQuery) {
+export default function buildPaginationQuery(query: PaginationQuery) {
   const { sort = 'id asc', skip = 0, limit = 20 } = query;
   return {
     ...query,
