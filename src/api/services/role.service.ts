00678d44be4d65c81ea53b69f0a6265733ac837e
Victor Garcia <vikgm.dev@gmail.com>
2020-04-04 00:24:12 -0600

    Fix role find method

--- src/api/services/role.service.ts
+++ src/api/services/role.service.ts
@@ -1,11 +1,45 @@
 import { PaginationQuery, RoleFilter } from '../types';
 import { Role } from '../models';
 
+const getFiltersQuery = (queryParam: RoleFilter) => {
+  let filters = {};
+  let orQuery = [];
+
+  if (queryParam) {
+    const query = queryParam;
+
+    if (query.title) {
+      const filter = {
+        title: {
+          contains: query.title,
+        },
+      };
+      orQuery.push(filter);
+    }
+
+    if (orQuery.length > 0) {
+      filters = {
+        or: orQuery,
+      };
+    }
+  }
+
+  return filters;
+};
+
 export const find = async (filter: RoleFilter, paginationQuery: PaginationQuery): Promise<any> => {
-  const entities = await Role.find()
+  // Run the query
+  const totalCount = await Role.estimatedDocumentCount();
+
+  const items = await Role.find(getFiltersQuery(filter))
+    .populate('permissions')
     .sort(paginationQuery.sort)
     .skip(paginationQuery.skip)
     .limit(paginationQuery.limit)
     .exec();
-  return entities;
+
+  return {
+    items,
+    totalCount,
+  };
 };
