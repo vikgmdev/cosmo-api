50b485696547016606efd0d9b67be5b4d2da0a25
Victor Garcia <vikgm.dev@gmail.com>
2020-04-04 00:09:34 -0600

    Implement update password with tokenservice

---
+++ src/api/services/auth/update-password-and-login.ts
@@ -0,0 +1,32 @@
+import { Request } from 'express';
+import { Helpers } from '../../../helpers';
+import { User } from '../../models';
+
+export const updatePasswordAndLogin = async (password: string, token: string, req: Request): Promise<any> => {
+  if (!token) {
+    throw 'invalidToken';
+  }
+
+  // Look up the user with this reset token.
+  var userRecord = await User.findOne({ passwordResetToken: token });
+
+  // If no such user exists, or their token is expired, bail.
+  if (!userRecord || userRecord.passwordResetTokenExpiresAt <= Date.now()) {
+    throw 'invalidToken';
+  }
+
+  // Hash the new password.
+  var hashed = await Helpers.utils.passwords.hashPassword(password);
+
+  // Store the user's new password and clear their reset token so it can't be used again.
+  await User.findByIdAndUpdate(userRecord.id, {
+    password: hashed,
+    passwordResetToken: '',
+    passwordResetTokenExpiresAt: 0,
+  });
+
+  // Log the user in.
+  req.me.id = userRecord.id;
+
+  return { message: 'The password was successfully updated.' };
+};
