be0ce6c0a76c8dd16de9c4ef7ce3fd2714534d6c
Victor Garcia <vikgm.dev@gmail.com>
2020-04-03 19:22:58 -0600

    Add role and permission routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,10 +1,14 @@
 import { Application } from 'express';
 import AuthRoutes from './auth/auth.routes';
 import HealthRoutes from './health/health.routes';
+import PermissionRoutes from './permission/permission.routes';
+import RoleRoutes from './role/role.routes';
 import UserRoutes from './user/user.routes';
 
 export default function routes(app: Application): void {
   app.use('/auth', AuthRoutes);
   app.use('/health', HealthRoutes);
+  app.use('/permission', PermissionRoutes);
+  app.use('/role', RoleRoutes);
   app.use('/user', UserRoutes);
 }
