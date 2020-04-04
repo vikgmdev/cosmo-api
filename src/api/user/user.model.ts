cfd4c0fa073dc8264cfa12c03c2db58177f2ad81
Victor Garcia <vikgm.dev@gmail.com>
2020-04-03 23:25:19 -0600

    Add new properties to User to track sessions

--- src/api/user/user.model.ts
+++ src/api/user/user.model.ts
@@ -7,6 +7,14 @@ export interface UserModel extends mongoose.Document {
   password: string;
   fullName: string;
   roles: RoleModel[];
+  passwordResetToken: string;
+  passwordResetTokenExpiresAt: number;
+  emailProofToken: string;
+  emailProofTokenExpiresAt: number;
+  emailStatus: string;
+  emailChangeCandidate: string;
+  tosAcceptedByIp: string;
+  lastSeenAt: number;
 }
 
 const userSchema = new mongoose.Schema(
@@ -26,6 +34,69 @@ const userSchema = new mongoose.Schema(
       required: true,
       maxlength: 120,
     },
+
+    // AUTH DATA
+    /**
+     * A unique token used to verify the user's identity when recovering a password.
+     * Expires after 1 use, or after a set amount of time has elapsed.
+     */
+    passwordResetToken: {
+      type: String,
+    },
+    /**
+     * A JS timestamp (epoch ms) representing the moment when this user's `passwordResetToken`
+     * will expire (or 0 if the user currently has no such token).
+     */
+    passwordResetTokenExpiresAt: {
+      type: Number,
+    },
+    /**
+     * A pseudorandom, probabilistically-unique token for use in our account verification emails.
+     */
+    emailProofToken: {
+      type: String,
+    },
+    /**
+     * A JS timestamp (epoch ms) representing the moment when this user's `emailProofToken`
+     * will expire (or 0 if the user currently has no such token).
+     */
+    emailProofTokenExpiresAt: {
+      type: Number,
+    },
+    /**
+     * The confirmation status of the user's email address.
+     * Users might be created as "unconfirmed" (e.g. normal signup) or as "confirmed" (e.g. hard-coded
+     * admin users).  When the email verification feature is enabled, new users created via the
+     * signup form have \`emailStatus: 'unconfirmed'\` until they click the link in the confirmation email.
+     * Similarly, when an existing user changes their email address, they switch to the "changeRequested"
+     * email status until they click the link in the confirmation email.
+     */
+    emailStatus: {
+      type: String,
+      enum: ['unconfirmed', 'changeRequested', 'confirmed'],
+      default: 'confirmed',
+    },
+    /**
+     * The (still-unconfirmed) email address that this user wants to change to.
+     */
+    emailChangeCandidate: {
+      type: String,
+    },
+    /**
+     * The IP (ipv4) address of the request that accepted the terms of service.
+     * Useful for certain types of businesses and regulatory requirements (KYC, etc.)
+     * moreInfoUrl: https://en.wikipedia.org/wiki/Know_your_customer
+     */
+    tosAcceptedByIp: {
+      type: String,
+    },
+    /**
+     * A JS timestamp (epoch ms) representing the moment at which this user most recently
+     * interacted with the backend while logged in (or 0 if they have not interacted with the backend at all yet).
+     */
+    lastSeenAt: {
+      type: Number,
+    },
     roles: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -36,7 +107,20 @@ const userSchema = new mongoose.Schema(
   {
     toJSON: {
       transform: (doc, ret): any => {
-        return omit(['password'], ret);
+        return omit(
+          [
+            'password',
+            'passwordResetToken',
+            'passwordResetTokenExpiresAt',
+            'emailProofToken',
+            'emailProofTokenExpiresAt',
+            'emailStatus',
+            'emailChangeCandidate',
+            'tosAcceptedByIp',
+            'lastSeenAt',
+          ],
+          ret,
+        );
       },
     },
   },
