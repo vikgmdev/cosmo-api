40689884a0067ab9c743990b10ac86638619f63f
Victor Garcia <vikgm.dev@gmail.com>
2020-04-03 23:22:42 -0600

    Add util to hash and check password

---
+++ src/helpers/utils/passwords.ts
@@ -0,0 +1,9 @@
+import crypto from 'crypto';
+
+export async function hashPassword(password: string): Promise<string> {
+  return crypto.createHash('sha256').update(password).digest('hex');
+}
+
+export async function checkPassword(password: string, hashedPassword: string): Promise<boolean> {
+  return await hashPassword(password) === hashedPassword;
+}
