aad70ccdf808a55cf6f2f7768365cbe3eb00db94
Victor Garcia <vikgm.dev@gmail.com>
2020-04-04 00:24:25 -0600

    Fix permission find method

--- src/api/services/permission.service.ts
+++ src/api/services/permission.service.ts
@@ -2,11 +2,19 @@ import { PaginationQuery } from '../types';
 import { Permission } from '../models';
 import { PermissionFilter } from '../types/permission.types';
 
-export const find = async (filter: PermissionFilter, paginationQuery: PaginationQuery): Promise<any> => {
-  const entities = await Permission.find()
+export const find = async (_filter: PermissionFilter, paginationQuery: PaginationQuery): Promise<any> => {
+  // Run the query
+  const totalCount = await Permission.estimatedDocumentCount();
+
+  const items = await Permission.find()
+    .populate('childrens')
     .sort(paginationQuery.sort)
     .skip(paginationQuery.skip)
     .limit(paginationQuery.limit)
     .exec();
-  return entities;
+
+  return {
+    items,
+    totalCount,
+  };
 };
