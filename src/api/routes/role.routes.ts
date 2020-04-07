6251bda5ec355a7513c487c85d9e74cdd1a5aaff
Victor Garcia <vikgm.dev@gmail.com>
2020-04-07 13:11:57 -0500

    Add delete role endpoint

--- src/api/routes/role.routes.ts
+++ src/api/routes/role.routes.ts
@@ -5,6 +5,7 @@ export default express
   .Router()
   .get('/', RoleController.find)
   .get('/:id', RoleController.getById)
+  .delete('/:id', RoleController.deleteById)
   .post('/', RoleController.create)
   .get('/:id/permissions', RoleController.getRolePermissions)
   .put('/:id/permissions', RoleController.updateRolePermissions);
