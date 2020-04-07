e12f1241eaad4f2544aca6c3e5f37eadd40bf253
Victor Garcia <vikgm.dev@gmail.com>
2020-04-07 11:37:16 -0500

    Remove user/me method from service

--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -54,16 +54,3 @@ export const find = async (filter: UserFilter, paginationQuery: PaginationQuery)
     totalCount,
   };
 };
-
-export const me = async (req: Request): Promise<any> => {
-  const token = await Helpers.token.getTokenFromRequest(req);
-
-  if (!token) throw 'tokenNotPresent';
-
-  // validate the token
-  const user = await Helpers.token.validateJwtToken(token);
-
-  if (!user) throw 'tokenInvalid';
-
-  return user;
-};
