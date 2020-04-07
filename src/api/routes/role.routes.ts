0c5d2c196bff6a08cf732ac2cd5c9b25fc931046
Victor Garcia <vikgm.dev@gmail.com>
2020-04-07 10:51:02 -0500

    Add get role permissions route

--- src/api/routes/role.routes.ts
+++ src/api/routes/role.routes.ts
@@ -1,4 +1,7 @@
 import express from 'express';
 import { RoleController } from '../controllers';
 
-export default express.Router().get('/find', RoleController.find);
+export default express
+  .Router()
+  .get('/find', RoleController.find)
+  .get('/:roleId/permissions', RoleController.getRolePermissions);
