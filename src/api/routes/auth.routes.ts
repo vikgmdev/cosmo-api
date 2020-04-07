9b116e66a434f9905eda4ccae82c24353d452fb8
Victor Garcia <vikgm.dev@gmail.com>
2020-04-07 11:36:13 -0500

    Add new auth/me endpoint

--- src/api/routes/auth.routes.ts
+++ src/api/routes/auth.routes.ts
@@ -4,6 +4,7 @@ import { AuthController } from '../controllers';
 export default express
   .Router()
   .get('/email/confirm', AuthController.confirmEmail)
+  .get('/me', AuthController.me)
   .get('/send-password-recovery-email', AuthController.sendPasswordRecoveryEmail)
   .post('/signup', AuthController.signup)
   .post('/update-password-and-login', AuthController.updatePasswordAndLogin)
