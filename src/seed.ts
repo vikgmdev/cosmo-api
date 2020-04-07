2a147a2f5a90fca9efc0ab99d36dc6cd61233f58
Victor Garcia <vikgm.dev@gmail.com>
2020-04-07 10:47:40 -0500

    Add seeds scripts for roles and permissions

---
+++ src/seed.ts
@@ -0,0 +1,96 @@
+import { Permission, Role } from './api/models';
+
+async function populatePermissions() {
+  const result = await new Permission({
+    title: 'App module',
+    name: 'accessToAppModule',
+    level: 1,
+    isSelected: false,
+  }).save();
+  const childs = await Promise.all([
+    new Permission({
+      title: 'Read',
+      name: 'canReadAppData',
+      level: 2,
+      isSelected: false,
+      parent: result.id
+    }).save(),
+    new Permission({
+      title: 'Edit',
+      name: 'canEditAppData',
+      level: 2,
+      isSelected: false,
+      parent: result.id
+    }).save(),
+    new Permission({
+      title: 'Delete',
+      name: 'canDeleteAppData',
+      level: 2,
+      isSelected: false,
+      parent: result.id
+    }).save(),
+  ]);
+  result.childrens = childs.map( per => per.id)
+  result.save();
+}
+
+async function populateAuthPermissions() {
+  const result = await new Permission({
+    title: 'Users Management module',
+    name: 'accessToAuthModule',
+    level: 1,
+    isSelected: false,
+  }).save();
+  const childs = await Promise.all([
+    new Permission({
+      title: 'Read',
+      name: 'canReadAuthData',
+      level: 2,
+      isSelected: false,
+      parent: result.id
+    }).save(),
+    new Permission({
+      title: 'Edit',
+      name: 'canEditAuthData',
+      level: 2,
+      isSelected: false,
+      parent: result.id
+    }).save(),
+    new Permission({
+      title: 'Delete',
+      name: 'canDeleteAuthData',
+      level: 2,
+      isSelected: false,
+      parent: result.id
+    }).save(),
+  ]);
+  result.childrens = childs.map( per => per.id)
+  result.save();
+}
+
+async function populateRoles() {
+  const roles = await Promise.all([
+    new Role({
+      title: 'Administrator',
+      isCoreRole: true,
+      isDefaultRole: false
+    }).save(),
+    new Role({
+      title: 'User',
+      isCoreRole: false,
+      isDefaultRole: true
+    }).save(),
+    new Role({
+      title: 'Manager',
+      isCoreRole: false,
+      isDefaultRole: false
+    }).save(),
+  ]);
+  console.log(roles);
+}
+
+export default {
+  populatePermissions,
+  populateAuthPermissions,
+  populateRoles
+};
