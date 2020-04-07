463352adf49cd714ef3b78c00fb3774563aee691
Victor Garcia <vikgm.dev@gmail.com>
2020-04-07 10:47:22 -0500

    Add seed function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import './core/env';
 import { Config } from './config';
 import { logger } from './core';
 import app from './app';
+import seeds from './seed';
+
+const seed = process.argv.find((arg) => arg === 'seed');
 
 app.listen({ port: Config.app.port }, (): void => {
   logger.info(`🚀 ${Config.app.name} up and running in ${Config.app.env} @ http://localhost:${Config.app.port}`);
+  if (seed) {
+    seeds.populatePermissions();
+    seeds.populateAuthPermissions();
+    seeds.populateRoles();
+  }
 });
