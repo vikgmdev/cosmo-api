7b062d1f0ecd5f231209e925b7506b45f7ba6cca
Victor Garcia <vikgm.dev@gmail.com>
2020-04-09 12:27:34 -0500

    Add Appointment model

--- src/api/models/appointment.model.ts
+++ src/api/models/appointment.model.ts
@@ -27,6 +27,7 @@ const appointmentSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
+    required: true,
   },
   zoomLink: {
     type: 'string',
