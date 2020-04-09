e4cbe3102dc70c0d81f07d01aaee7b7c6beadc57
Victor Garcia <vikgm.dev@gmail.com>
2020-04-08 19:16:35 -0500

    Implement isLoggedIn policy on routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,11 +4,12 @@ import HealthRoutes from './routes/health.routes';
 import PermissionRoutes from './routes/permission.routes';
 import RoleRoutes from './routes/role.routes';
 import UserRoutes from './routes/user.routes';
+import { isLoggedIn } from './middlewares';
 
 export default function routes(app: Application): void {
   app.use('/auth', AuthRoutes);
-  app.use('/health', HealthRoutes);
-  app.use('/permission', PermissionRoutes);
-  app.use('/role', RoleRoutes);
-  app.use('/user', UserRoutes);
+  app.use('/health', isLoggedIn, HealthRoutes);
+  app.use('/permission', isLoggedIn, PermissionRoutes);
+  app.use('/role', isLoggedIn, RoleRoutes);
+  app.use('/user', isLoggedIn, UserRoutes);
 }
