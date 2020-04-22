6e458bb84a899532eaf15098d0b82cec33dafca4
Victor Garcia <vikgm.dev@gmail.com>
2020-04-21 19:10:32 -0500

    Add basic cosmobilogy endpoints

--- src/api/routes/cosmobiologia.routes.ts
+++ src/api/routes/cosmobiologia.routes.ts
@@ -1,4 +1,8 @@
 import express from 'express';
 import { CosmobiologiaController } from '../controllers';
 
-export default express.Router().get('/natal', CosmobiologiaController.natal);
+export default express
+  .Router()
+  .get('/natal', CosmobiologiaController.natal)
+  .get('/progresado', CosmobiologiaController.progresado)
+  .get('/life', CosmobiologiaController.life);
