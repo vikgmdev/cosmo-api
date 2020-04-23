04f46f0ea91eb7cc77e86ae5923f12badc60b92e
Victor Garcia <vikgm.dev@gmail.com>
2020-04-23 12:49:43 -0500

    Add cors middleware to express

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 import { Config } from './config';
 import routes from './api/routes';
 import { connectDatabase } from './core';
@@ -11,6 +12,11 @@ app.use(
   bodyParser.urlencoded({
     extended: true,
     limit: Config.app.requestLimit,
+  }),
+);
+app.use(
+  cors({
+    origin: 'http://localhost:4200',
   }),
 );
 routes(app);
